9541162b067f203d0d8ec508f4900710df4639cb
Lucas Rafael Leite <lucasrafaelleite@gmail.com>
2024-01-11 22:06:21 -0300

    init contact

--- js/home.js
+++ js/home.js
@@ -1,27 +1,31 @@
 /* Vantagens */
-var splide_vantagens = new Splide( '.lista__vantagens', {
-    type : 'loop',
-    perPage: 4,
-    gap: 60,
-    breakpoints: {
-        640: {
+if (document.querySelector('.lista__vantagens')) {
+    var splide_vantagens = new Splide( '.lista__vantagens', {
+        type : 'loop',
+        perPage: 4,
+        gap: 60,
+        breakpoints: {
+            640: {
+            },
         },
-    },
-})
-splide_vantagens.mount();
+    })
+    splide_vantagens.mount();
+}
 
 
 /* Blog */
-var splide__blog = new Splide( '.blog__list', {
-    perPage: 2,
-    gap: 15,
-    autoWidth: true,
-    breakpoints: {
-        640: {
+if (document.querySelector('.blog__list')) {
+    var splide__blog = new Splide( '.blog__list', {
+        perPage: 2,
+        gap: 15,
+        autoWidth: true,
+        breakpoints: {
+            640: {
+            },
         },
-    },
-})
-splide__blog.mount();
+    })
+    splide__blog.mount();
+}
 
 
 
