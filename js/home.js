0d455f6882c25cc127a2807f1f3e860c6d91a25d
Lucas Rafael Leite <lucasrafaelleite@gmail.com>
2024-01-23 00:20:09 -0300

    updates and blog

--- js/home.js
+++ js/home.js
@@ -14,18 +14,57 @@ if (document.querySelector('.lista__vantagens')) {
 
 
 /* Blog */
-if (document.querySelector('.blog__list')) {
-    var splide__blog = new Splide( '.blog__list', {
-        perPage: 2,
-        gap: 15,
-        autoWidth: true,
-        breakpoints: {
-            640: {
-            },
-        },
-    })
-    splide__blog.mount();
+
+/* Blog */
+if (document.querySelector('.blog')) {
+    document.querySelector('.blog__list .splide__list').innerHTML = '<img src="../img/loading.gif" style="display:block; width:50px; margin:auto; position:absolute; left:50%; margin-left:-25px">';
+    function fetchPosts() {
+        fetch('https://blog.bughunt.com.br/ghost/api/v3/content/posts/?key=aae82e19f1c2be980863c0bb5f')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            var contador = 0;
+            var items = '';
+            for (const post of data.posts) {
+                if (contador <= 6) {
+                    items += `
+                        <li class="splide__slide">
+                            <div class="blog__post">
+                                <a href="${post.url}" target="_blank" rel="noopener noreferrer"></a>
+                                <img src="${post.feature_image}" alt="${post.title}">
+                                <div class="blog__text">
+                                    <small>BugHunt</small>
+                                    <h4>${post.title}</h4>
+                                </div>                    
+                            </div>
+                        </li>
+                    `;   
+                }
+                contador++;
+                document.querySelector('.blog__list .splide__list').innerHTML = items;
+            }
+
+            var splide__blog = new Splide( '.blog__list', {
+                perPage: 2,
+                gap: 15,
+                autoWidth: true,
+                breakpoints: {
+                    640: {
+                    },
+                },
+            })
+            splide__blog.mount();
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    setTimeout(() => {
+        fetchPosts();
+    }, 1000);   
 }
+
 
 
 
