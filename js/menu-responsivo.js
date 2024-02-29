9affdd3594b227810ac62cac55ba48a2d3ac20a8
Lucas Rafael Leite <lucasrafaelleite@gmail.com>
2024-02-29 00:17:56 -0300

    fix menu

--- js/menu-responsivo.js
+++ js/menu-responsivo.js
@@ -1,13 +1,11 @@
-setTimeout(() => {
+window.addEventListener("load", (event) => {
 
-    var menu = document.querySelector('header .menu ul');
-    document.querySelector('.menu__responsivo .menu ul').innerHTML = menu.innerHTML;
-    document.querySelector('.menu__responsivo .menu ul').innerHTML = document.querySelector('.menu__responsivo .menu ul').innerHTML + '<li><a href="contato">Fale conosco</a></li>';
-    document.querySelector('.menu__responsivo .menu ul');
+    var menu = document.querySelector('header .menu ul').innerHTML;
+    document.querySelector('.menu__responsivo .menu ul').innerHTML = menu + '<li><a href="contato">Fale Conosco</a></li>';
 
     document.querySelector('.openResponsivo').addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector('.menu__responsivo').classList.toggle('ativo');
         document.querySelector('.openResponsivo').classList.toggle('ativo');
     })
-}, 100);
+})
